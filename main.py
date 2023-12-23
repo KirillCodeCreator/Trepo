106591eb8fc1805089040a9d6b5393bba171e2b0
Лоторев Кирилл <151776260+KirillCodeCreator@users.noreply.github.com>
2023-12-23 13:14:14 +0300

    Экспрессо

--- main.py
+++ main.py
@@ -6,37 +6,10 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QMessageBox, QDialog
 
 
-class EditDB(QDialog):
-    def __init__(self):
-        super(EditDB, self).__init__()
-        uic.loadUi("addEditCoffeeForm.ui", self)
-
-    def accept(self) -> None:
-        con = sqlite3.connect('coffee.sqlite')
-        cur = con.cursor()
-        cur.execute('INSERT INTO coffee(Name, Roasting, Type, Taste, Price, Size) VALUES (?,?,?,?,?,?)',
-                    [self.NameLineEdit.text(), self.RoastingLineEdit.text(), self.TypeLineEdit.text(),
-                     self.TasteLineEdit.text(), self.PriceLineEdit.text(), self.SizeLineEdit.text()])
-        con.commit()
-        con.close()
-        self.done(0)
-
-    def reject(self) -> None:
-        self.done(0)
-
-
 class Window(QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
         uic.loadUi("main.ui", self)
-        self.table()
-        self.addInfoButton.clicked.connect(self.run)
-
-    def run(self) -> None:
-        sys.excepthook = except_hook
-        EDB = EditDB()
-        EDB.show()
-        EDB.exec()
         self.table()
 
     def table(self) -> None:
@@ -46,11 +19,12 @@ class Window(QMainWindow):
         db = cur.fetchall()
         self.tableWidget.setColumnCount(len(db[0]) - 1)
         self.tableWidget.setRowCount(len(db))
-        self.tableWidget.setHorizontalHeaderLabels(["Name", "Roasting", "Type", "Taste", "Price", "Size"])
+        self.tableWidget.setHorizontalHeaderLabels(["Наименование", "Степень обжарки", "Тип", "Вкус", "Цена", "Объём"])
         for i, elemi in enumerate(db):
             for j, elemj in enumerate(elemi[1:]):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(elemj))
         con.close()
+
 
 
 def except_hook(cls, exception, trace):
@@ -58,6 +32,7 @@ def except_hook(cls, exception, trace):
     print(tb)
 
 
+# Точка входа (Entry point)
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     sys.excepthook = except_hook
